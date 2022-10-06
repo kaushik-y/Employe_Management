7d6cbcb4f1fcbe6dbf80b1a9bfe0087dfa0c8b4d
Kaushik Radadiya <kaushikradadiya260@gmail.com>
2022-10-06 22:35:47 +0530

    final commit

---
+++ src/main/java/com/example/Employe_manage/EmployeManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employe_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Employe_manage/EmployeManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Employe_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
